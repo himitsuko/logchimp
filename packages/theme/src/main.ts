b7ab54d2c9c924620fe449e0e16a0174298bb3cd
Himitsuko <himitsuko@github.com>
2023-06-27 20:52:54 +0700

    chore: rw url

--- packages/theme/src/main.ts
+++ packages/theme/src/main.ts
@@ -6,6 +6,9 @@ import App from "./App.vue";
 import { router } from "./router";
 
 import "./styles/main.sass";
+import axios from "axios";
+
+//axios.defaults.baseURL = "https://drapi.up.railway.app/"
 
 const app = createApp(App);
 const store = createPinia();
